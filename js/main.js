e26beaf469ce1c92e262803edb6d6315954bfbf6
Garieldas <dainiusrainys99@gmail.com>
2025-02-17 22:23:28 +0200

    created array for number buttons and operations. Created functions: add,minus,substract,multiply and reset

--- js/main.js
+++ js/main.js
@@ -1,14 +1,101 @@
-const screen = document.querySelector('#scren');
+const screen = document.querySelector('#screen');
+// Select div buttons
 const buttons = document.querySelector('#buttons');
+// Get all buttons elements from that div
 const buttonsList = buttons.querySelectorAll('button');
+// Convert Nodelist to an actuall array to use array methods
+const newList = Array.from(buttonsList);
+
+const specialKeys = ['+', '-', '*', '/', '=', 'C'];
+const operatorKeys = ['+', '-', '*', '/'];
+
+// All buttons that contains numbers array creation
+const numbers = newList.filter(number => (
+    !specialKeys.includes(number.textContent)
+));
+
+const operators = newList.filter(operator => (
+    operatorKeys.includes(operator.textContent)
+));
+
+const equal = document.querySelector('#equalBtn');
+const clear = document.querySelector('#clearBtn');
+
+let num1 = 0;
+let operation = 0;
+let num2 = 0;
 
 let getValue = (e) => {
     let value = e.target.textContent;
-    console.log(value);
+    displayValue(value);
     return value;
 }
 
-buttonsList.forEach((btn) => {
-    btn.addEventListener("click", getValue);
+let displayValue = (value) => {
+
+    let prev = screen.value;
+    let current = value;
+
+    screen.value = `${prev}${current}`;
+}
+
+let firstNum = (e) => {
+
+    num1 = screen.value;
+    operation = e.target.textContent;
+    screen.value = '';
+    // console.log(num1);
+    // console.log(operation);
+
+}
+
+let result = () => {
+    num2 = screen.value;
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    switch (operation) {
+        case '+':
+            answer=sum(num1, num2);
+            break;
+        case '-':
+            answer=minus(num1, num2);
+            break;
+        case '*':
+            answer=multiply(num1, num2);
+            break;
+        case '/':
+            answer=substract(num1, num2);
+            break;
+    }
+    screen.value = answer;
+}
+
+let sum = (num1, num2) => num1 + num2;
+
+let minus = (num1, num2) => num1 - num2;
+
+let multiply = (num1, num2) => num1 * num2;
+
+let substract = (num1, num2) => num1 / num2;
+
+let reset = () => {
+    num1 = '';
+    num2 = '';
+    operation = '';
+    screen.value = '';
+}
+
+numbers.forEach((number) => {
+    number.addEventListener("click", getValue);
     
 });
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (e) => {
+        firstNum(e);
+})
+});
+
+equal.addEventListener("click", result);
+
+clear.addEventListener("click", reset);
